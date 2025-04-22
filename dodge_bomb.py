10940c3d5b7a31d9cffe9b8c0220afd927577836
c0a2413053 <c0a2413053@edu.teu.ac.jp>
2025-04-22 14:55:30 +0900

    練習4:爆弾にぶつかると終了

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -53,6 +53,10 @@ def main():
                 return
         screen.blit(bg_img, [0, 0]) 
 
+        if kk_rct.colliderect(bb_rct):
+            print("gameover")
+            return
+
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key, mv in DELTA.items():
